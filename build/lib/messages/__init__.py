6a519b18d36b51952c6ecfad8b979824b9ef9e2a
Mark Danico A. Fernandez <nico@serverless.ninja>
2024-02-16 10:20:32 +0800

    Remove build for now

--- build/lib/messages/__init__.py
+++
@@ -1,7 +0,0 @@
-#messages directory
-DESCRIPTION = "PySERFF CLI, for creating microservices using Serverless Framework for your application."
-BANNER = f"""\
-    {DESCRIPTION}
-    Use -h or --help to see commands available.
-"""
-                                    
